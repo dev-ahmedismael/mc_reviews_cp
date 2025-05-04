265c335cd74a7a27a9548e61d60f53c8a37dc914
Ahmed Ismael <dev.ahmedismael@outlook.com>
2025-05-04 17:23:54 +0300

    pre

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -64,7 +64,7 @@ export class LoginComponent {
               this.messageService.add({
                 severity: 'error',
                 summary: 'خطأ في تسجيل الدخول',
-                detail: err.error.message,
+                detail: err.error.message || '',
               });
             },
           });
